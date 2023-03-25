6c0c69b8e6d70df6dbaed9e1b5fa6975702c132b
Syed Ubaid Ali Jafri <33892624+Ubaidjaffery@users.noreply.github.com>
2023-03-25 14:01:20 +0500

    Share Point Enumeration
    Assess SharePoint against OneDrive enumeration.

---
+++ CheckSharepoint.ps1
@@ -0,0 +1,20 @@
+﻿$domain = "example.sharepoint.com" # replace with your target domain
+$vulnUrls = @("/_vti_bin/owssvr.dll", "/_vti_bin/shtml.exe", "/_vti_bin/_vti_aut/author.dll","/_layouts/15/onedrive.aspx", "/personal") # list of vulnerable URLs to check
+$results = @() # initialize an empty array to store the results
+
+foreach ($url in $vulnUrls) {
+    $fullUrl = "https://$domain$url"
+    try {
+        $response = Invoke-WebRequest $fullUrl -UseBasicParsing -ErrorAction Stop
+        $status = $response.StatusCode
+    } catch {
+        $status = $_.Exception.Response.StatusCode.Value__
+    }
+    $result = [PSCustomObject]@{
+        URL = $fullUrl
+        Status = $status
+    }
+    $results += $result
+}
+
+$results | Format-Table -AutoSize
